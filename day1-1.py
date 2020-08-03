9d6c983ab6383bc1144a2a2a294e6184543a55df
Ethan200998 <69131929+Ethan200998@users.noreply.github.com>
2020-08-03 11:17:17 +0800

    Add files via upload

---
+++ day1-1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:07:11 2020
+
+@author: AE401
+"""
+
+print('I\'m Eric. \nHe is "cool".')
---
+++ day1-2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:35:33 2020
+
+@author: AE401
+"""
+
+name = input ('請問你的名子叫什麼?')
+print("Hellow",name)             
